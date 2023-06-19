Linutronix/detnetctl

commit 14dbd1b1e5e5fb1d2141f2a26adba570dd4efdfb

Make registration async
In order to call async functions during the registration
process, the registration needs to be async itself.
Due to the nature of futures, it is necessary to use
Arc<Mutex<...>> instead of Box<...> for injecting the
different components.

Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>
Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

Cargo.toml
@@ -25,6 +25,8 @@ serde_yaml = { version = "0.9" }
 
 clap = { version = "4.2.0", features = ["derive","wrap_help","cargo"] }
 
+futures = "0.3.11"
+
 ### dbus feature
 # From github to include the set_sender feature just merged TODO replace when released
 dbus = { git = "https://github.com/diwic/dbus-rs", branch = "master", optional = true }

src/main.rs
@@ -2,8 +2,10 @@ extern crate detnetctl;
 
 use anyhow::{anyhow, Error, Result};
 use clap::Parser;
+use futures::lock::Mutex;
 use std::fs::File;
 use std::path::PathBuf;
+use std::sync::Arc;
 
 use detnetctl::configuration::{Configuration, YAMLConfiguration};
 use detnetctl::controller::{Controller, Registration};
@@ -40,22 +42,22 @@ pub async fn main() -> Result<()> {
 
     let cli = Cli::parse();
 
-    let mut configuration = match cli.config {
+    let configuration = match cli.config {
         Some(file) => {
-            let mut c = Box::new(YAMLConfiguration::new());
+            let mut c = YAMLConfiguration::new();
             c.read(File::open(file)?)?;
-            c
+            Arc::new(Mutex::new(c))
         }
         None => new_sysrepo_config()?,
     };
 
-    let mut queue_setup = match cli.no_queue_setup {
-        Some(priority) => Box::new(DummyQueueSetup::new(priority)),
+    let queue_setup = match cli.no_queue_setup {
+        Some(priority) => Arc::new(Mutex::new(DummyQueueSetup::new(priority))),
         None => new_detd_gateway()?,
     };
 
-    let mut guard = match cli.no_guard {
-        true => Box::new(DummyGuard::new()),
+    let guard = match cli.no_guard {
+        true => Arc::new(Mutex::new(DummyGuard::new())),
         false => new_bpf_guard(cli.bpf_debug_output)?,
     };
 
@@ -63,15 +65,25 @@ pub async fn main() -> Result<()> {
 
     match cli.app_name {
         Some(app_name) => {
-            let response = controller.register(
-                &app_name,
-                &mut *configuration,
-                &mut *queue_setup,
-                &mut *guard,
-            )?;
+            let response = controller
+                .register(
+                    &app_name,
+                    configuration,
+                    queue_setup,
+                    guard,
+                )
+                .await?;
             println!("Final result: {:#?}", response);
         }
-        None => spawn_dbus_service(controller, configuration, queue_setup, guard).await?,
+        None => {
+            spawn_dbus_service(
+                Arc::new(Mutex::new(controller)),
+                configuration,
+                queue_setup,
+                guard,
+            )
+            .await?
+        }
     }
 
     Ok(())
@@ -90,17 +102,33 @@ use {
 };
 #[cfg(feature = "dbus")]
 async fn spawn_dbus_service(
-    controller: Controller,
-    mut configuration: Box<dyn Configuration + Send>,
-    mut queue_setup: Box<dyn QueueSetup + Send>,
-    mut guard: Box<dyn Guard + Send>,
+    controller: Arc<Mutex<Controller>>,
+    configuration: Arc<Mutex<dyn Configuration + Send>>,
+    queue_setup: Arc<Mutex<dyn QueueSetup + Send>>,
+    guard: Arc<Mutex<dyn Guard + Send>>,
 ) -> Result<()> {
     let shutdown = Shutdown::new();
     let mut facade = Facade::new(shutdown.clone())?;
 
     facade
         .setup(Box::new(move |app_name| {
-            controller.register(app_name, &mut *configuration, &mut *queue_setup, &mut *guard)
+            let app_name = String::from(app_name);
+            let cloned_controller = controller.clone();
+            let cloned_configuration = configuration.clone();
+            let cloned_queue_setup = queue_setup.clone();
+            let cloned_guard = guard.clone();
+            Box::pin(async move {
+                cloned_controller
+                    .lock()
+                    .await
+                    .register(
+                        &app_name,
+                        cloned_configuration,
+                        cloned_queue_setup,
+                        cloned_guard,
+                    )
+                    .await
+            })
         }))
         .await?;
 
@@ -124,7 +152,7 @@ async fn spawn_dbus_service(
 
 #[cfg(not(feature = "dbus"))]
 async fn spawn_dbus_service(
-    _controller: Controller,
+    _controller: Arc<Mutex<Controller>>,
     mut _configuration: Box<dyn Configuration + Send>,
     mut _queue_setup: Box<dyn QueueSetup + Send>,
     mut _guard: Box<dyn Guard + Send>,
@@ -135,35 +163,35 @@ async fn spawn_dbus_service(
 #[cfg(feature = "bpf")]
 use detnetctl::guard::BPFGuard;
 #[cfg(feature = "bpf")]
-fn new_bpf_guard(debug_output: bool) -> Result<Box<dyn Guard + Send>> {
-    Ok(Box::new(BPFGuard::new(debug_output)))
+fn new_bpf_guard(debug_output: bool) -> Result<Arc<Mutex<dyn Guard + Send>>> {
+    Ok(Arc::new(Mutex::new(BPFGuard::new(debug_output))))
 }
 
 #[cfg(not(feature = "bpf"))]
-fn new_bpf_guard(_debug_output: bool) -> Result<Box<dyn Guard + Send>> {
+fn new_bpf_guard(_debug_output: bool) -> Result<Arc<Mutex<dyn Guard + Send>>> {
     Err(feature_missing_error("bpf", "--no-guard"))
 }
 
 #[cfg(feature = "sysrepo")]
 use detnetctl::configuration::SysrepoConfiguration;
 #[cfg(feature = "sysrepo")]
-fn new_sysrepo_config() -> Result<Box<dyn Configuration + Send>> {
-    Ok(Box::new(SysrepoConfiguration::new()?))
+fn new_sysrepo_config() -> Result<Arc<Mutex<dyn Configuration + Send>>> {
+    Ok(Arc::new(Mutex::new(SysrepoConfiguration::new()?)))
 }
 
 #[cfg(not(feature = "sysrepo"))]
-fn new_sysrepo_config() -> Result<Box<dyn Configuration + Send>> {
+fn new_sysrepo_config() -> Result<Arc<Mutex<dyn Configuration + Send>>> {
     Err(feature_missing_error("sysrepo", "--config"))
 }
 
 #[cfg(feature = "detd")]
 use detnetctl::queue_setup::DetdGateway;
 #[cfg(feature = "detd")]
-fn new_detd_gateway() -> Result<Box<dyn QueueSetup + Send>> {
-    Ok(Box::new(DetdGateway::new(None, None)?))
+fn new_detd_gateway() -> Result<Arc<Mutex<dyn QueueSetup + Send>>> {
+    Ok(Arc::new(Mutex::new(DetdGateway::new(None, None)?)))
 }
 
 #[cfg(not(feature = "detd"))]
-fn new_detd_gateway() -> Result<Box<dyn QueueSetup + Send>> {
+fn new_detd_gateway() -> Result<Arc<Mutex<dyn QueueSetup + Send>>> {
     Err(feature_missing_error("detd", "--no-queue-setup"))
 }

src/controller/mod.rs
@@ -14,13 +14,18 @@
 //! # let tmpfile = doctest::generate_example_yaml();
 //! # let filepath = tmpfile.path();
 //! use std::fs::File;
-//! #
+//! use std::sync::Arc;
+//! use futures::lock::Mutex;
+//!
+//! # tokio_test::block_on(async {
 //! let controller = Controller::new();
-//! let mut configuration = YAMLConfiguration::new();
-//! configuration.read(File::open(filepath)?)?;
-//! let mut queue_setup = DummyQueueSetup::new(3);
-//! let mut guard = DummyGuard::new();
-//! let response = controller.register("app0", &mut configuration, &mut queue_setup, &mut guard)?;
+//! let mut configuration = Arc::new(Mutex::new(YAMLConfiguration::new()));
+//! configuration.lock().await.read(File::open(filepath)?)?;
+//! let mut queue_setup = Arc::new(Mutex::new(DummyQueueSetup::new(3)));
+//! let mut guard = Arc::new(Mutex::new(DummyGuard::new()));
+//! let response = controller.register("app0", configuration, queue_setup, guard).await?;
+//! # Ok::<(), anyhow::Error>(())
+//! # });
 //! # Ok::<(), anyhow::Error>(())
 //! ```
 
@@ -28,7 +33,10 @@ use crate::configuration::Configuration;
 use crate::guard::Guard;
 use crate::queue_setup::QueueSetup;
 use anyhow::{Context, Result};
+use async_trait::async_trait;
+use futures::lock::Mutex;
 use getrandom::getrandom;
+use std::sync::Arc;
 
 /// The reponse of a registration to be passed back to the application
 /// for setting up the socket appropriately.
@@ -43,6 +51,7 @@ pub struct RegisterResponse {
 }
 
 /// Defines a registration operation
+#[async_trait]
 pub trait Registration {
     /// Register an application including the following steps
     ///
@@ -52,12 +61,12 @@ pub trait Registration {
     /// 4. Set up the guard to prevent interfering messages from other applications
     /// 5. Return the appropriate socket settings for the application
     ///
-    fn register(
+    async fn register(
         &self,
         app_name: &str,
-        configuration: &mut dyn Configuration,
-        queue_setup: &mut dyn QueueSetup,
-        guard: &mut dyn Guard,
+        mut configuration: Arc<Mutex<dyn Configuration + Send>>,
+        queue_setup: Arc<Mutex<dyn QueueSetup + Send>>,
+        mut guard: Arc<Mutex<dyn Guard + Send>>,
     ) -> Result<RegisterResponse>;
 }
 
@@ -79,13 +88,14 @@ impl Controller {
     }
 }
 
+#[async_trait]
 impl Registration for Controller {
-    fn register(
+    async fn register(
         &self,
         app_name: &str,
-        configuration: &mut dyn Configuration,
-        queue_setup: &mut dyn QueueSetup,
-        guard: &mut dyn Guard,
+        configuration: Arc<Mutex<dyn Configuration + Send>>,
+        queue_setup: Arc<Mutex<dyn QueueSetup + Send>>,
+        guard: Arc<Mutex<dyn Guard + Send>>,
     ) -> Result<RegisterResponse> {
         println!("Request to register {}", app_name);
 
@@ -94,6 +104,8 @@ impl Registration for Controller {
 
         // Fetch configuration for app
         let app_config = configuration
+            .lock()
+            .await
             .get_app_config(app_name)
             .context("Fetching the configuration failed")
             .map_err(|e| {
@@ -105,6 +117,8 @@ impl Registration for Controller {
 
         // Setup NIC
         let socket_config = queue_setup
+            .lock()
+            .await
             .apply_config(&app_config)
             .context("Setting up the queues failed")
             .map_err(|e| {
@@ -118,7 +132,8 @@ impl Registration for Controller {
         // here, because otherwise it would be possible to use a different logical interface,
         // but same SO_PRIORITY and physical interface. Even though it would not be routed to the
         // same VLAN it could still block the time slot!
-        guard
+        let mut locked_guard = guard.lock().await;
+        locked_guard
             .protect_priority(
                 &app_config.physical_interface,
                 socket_config.priority,
@@ -216,53 +231,60 @@ mod tests {
         guard
     }
 
-    #[test]
-    fn test_register_happy() -> Result<()> {
+    #[tokio::test]
+    async fn test_register_happy() -> Result<()> {
         let interface = "ethxy";
         let vid = 43;
         let priority = 32;
-        let mut configuration = configuration_happy(String::from(interface), vid);
-        let mut queue_setup = queue_setup_happy(priority);
-        let mut guard = guard_happy();
+        let configuration = Arc::new(Mutex::new(configuration_happy(
+            String::from(interface),
+            vid,
+        )));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(priority)));
+        let guard = Arc::new(Mutex::new(guard_happy()));
         let controller = Controller::new();
-        let response =
-            controller.register("app123", &mut configuration, &mut queue_setup, &mut guard)?;
+        let response = controller
+            .register("app123", configuration, queue_setup, guard)
+            .await?;
         assert_eq!(response.logical_interface, format!("{}.{}", interface, vid));
         assert_eq!(response.priority, priority);
         assert!(response.token > 10); // not GUARANTEED, but VERY unlikely to fail
         Ok(())
     }
 
-    #[test]
-    fn test_register_configuration_failure() {
-        let mut configuration = configuration_failing();
-        let mut queue_setup = queue_setup_happy(32);
-        let mut guard = guard_happy();
+    #[tokio::test]
+    async fn test_register_configuration_failure() {
+        let configuration = Arc::new(Mutex::new(configuration_failing()));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(32)));
+        let guard = Arc::new(Mutex::new(guard_happy()));
         let controller = Controller::new();
         assert!(controller
-            .register("app123", &mut configuration, &mut queue_setup, &mut guard)
+            .register("app123", configuration, queue_setup, guard)
+            .await
             .is_err());
     }
 
-    #[test]
-    fn test_register_queue_setup_failure() {
-        let mut configuration = configuration_happy(String::from("abc"), 4);
-        let mut queue_setup = queue_setup_failing();
-        let mut guard = guard_happy();
+    #[tokio::test]
+    async fn test_register_queue_setup_failure() {
+        let configuration = Arc::new(Mutex::new(configuration_happy(String::from("abc"), 4)));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_failing()));
+        let guard = Arc::new(Mutex::new(guard_happy()));
         let controller = Controller::new();
         assert!(controller
-            .register("app123", &mut configuration, &mut queue_setup, &mut guard)
+            .register("app123", configuration, queue_setup, guard)
+            .await
             .is_err());
     }
 
-    #[test]
-    fn test_register_guard_failure() {
-        let mut configuration = configuration_happy(String::from("abc"), 4);
-        let mut queue_setup = queue_setup_happy(32);
-        let mut guard = guard_failing();
+    #[tokio::test]
+    async fn test_register_guard_failure() {
+        let configuration = Arc::new(Mutex::new(configuration_happy(String::from("abc"), 4)));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(32)));
+        let guard = Arc::new(Mutex::new(guard_failing()));
         let controller = Controller::new();
         assert!(controller
-            .register("app123", &mut configuration, &mut queue_setup, &mut guard)
+            .register("app123", configuration, queue_setup, guard)
+            .await
             .is_err());
     }
 }

src/facade/dbus/mod.rs
@@ -1,3 +1,4 @@
+use crate::facade::RegisterCallback;
 use anyhow::{anyhow, Result};
 use async_shutdown::Shutdown;
 use async_trait::async_trait;
@@ -36,8 +37,6 @@ pub struct DBus {
     resource_handle: Arc<tokio::task::JoinHandle<()>>,
     shutdown: Shutdown,
 }
-
-type RegisterCallback = Box<dyn FnMut(&str) -> Result<controller::RegisterResponse> + Send>;
 
 impl DBus {
     pub fn new(shutdown: Shutdown) -> Result<Self> {
@@ -90,7 +89,7 @@ impl DBus {
                 )
                 .await
                 {
-                    Ok(()) => register(&cmd.app_name),
+                    Ok(()) => register(&cmd.app_name).await,
                     Err(e) => Err(e),
                 };
 
@@ -300,10 +299,12 @@ mod tests {
             let register_called_for_setup = register_called.clone();
             dbus.setup(Box::new(move |_| {
                 register_called_for_setup.store(true, Ordering::Relaxed);
-                Ok(controller::RegisterResponse {
-                    logical_interface: String::from(INTERFACE),
-                    priority: PRIORITY,
-                    token: TOKEN,
+                Box::pin(async move {
+                    Ok(controller::RegisterResponse {
+                        logical_interface: String::from(INTERFACE),
+                        priority: PRIORITY,
+                        token: TOKEN,
+                    })
                 })
             }))
             .await?;

src/facade/mod.rs
@@ -15,16 +15,30 @@
 //! #
 //! # tokio_test::block_on(async {
 //! use std::fs::File;
-//! let controller = Controller::new();
+//! use std::sync::Arc;
+//! use futures::lock::Mutex;
+//!
+//! let controller = Arc::new(Mutex::new(Controller::new()));
 //! let shutdown = Shutdown::new();
 //! let mut facade = Facade::new(shutdown.clone())?;
-//! let mut configuration = YAMLConfiguration::new();
-//! configuration.read(File::open(filepath)?)?;
-//! let mut queue_setup = DummyQueueSetup::new(3);
-//! let mut guard = DummyGuard::new();
+//! let mut configuration = Arc::new(Mutex::new(YAMLConfiguration::new()));
+//! configuration.lock().await.read(File::open(filepath)?)?;
+//! let mut queue_setup = Arc::new(Mutex::new(DummyQueueSetup::new(3)));
+//! let mut guard = Arc::new(Mutex::new(DummyGuard::new()));
 //!
 //! facade.setup(Box::new(move |app_name| {
-//!     controller.register(app_name, &mut configuration, &mut queue_setup, &mut guard)
+//!     let app_name = String::from(app_name);
+//!     let cloned_controller = controller.clone();
+//!     let cloned_configuration = configuration.clone();
+//!     let cloned_queue_setup = queue_setup.clone();
+//!     let cloned_guard = guard.clone();
+//!     Box::pin(async move {
+//!         cloned_controller.lock().await.register(
+//!             &app_name,
+//!             cloned_configuration,
+//!             cloned_queue_setup,
+//!             cloned_guard).await
+//!     })
 //! })).await?;
 //! # Ok::<(), anyhow::Error>(())
 //! # });
@@ -34,17 +48,19 @@ use crate::controller::RegisterResponse;
 use anyhow::Result;
 use async_shutdown::Shutdown;
 use async_trait::async_trait;
+use std::future::Future;
+use std::pin::Pin;
 
 mod dbus;
+
+type RegisterFuture = Pin<Box<dyn Future<Output = Result<RegisterResponse, anyhow::Error>> + Send>>;
+type RegisterCallback = Box<dyn for<'a> FnMut(&'a str) -> RegisterFuture + Send>;
 
 /// Setup of the facade
 #[async_trait]
 pub trait Setup {
     /// Setup the facade by providing a callback for the registration command
-    async fn setup(
-        &mut self,
-        register: Box<dyn for<'a> FnMut(&'a str) -> Result<RegisterResponse> + Send>,
-    ) -> Result<()>;
+    async fn setup(&mut self, register: RegisterCallback) -> Result<()>;
 }
 
 /// Represents the D-Bus facade
@@ -63,10 +79,7 @@ impl Facade {
 
 #[async_trait]
 impl Setup for Facade {
-    async fn setup(
-        &mut self,
-        register: Box<dyn for<'a> FnMut(&'a str) -> Result<RegisterResponse> + Send>,
-    ) -> Result<()> {
+    async fn setup(&mut self, register: RegisterCallback) -> Result<()> {
         self.dbus.setup(register).await?;
         Ok(())
     }

commit 7b9d15717024b9f53432467b96b1357ae0e189de

fix: Build with no-default-features
After 14dbd1b1e5e5 ("Make registration async"), it was
no longer possible to build all feature combinations.
Some minor changes were necessary to adapt to changed
interfaces.

Future similar refactorings should be tested with:

cargo hack check --feature-powerset --skip libbpf_with_sotoken
cargo hack test --feature-powerset --skip libbpf_with_sotoken

Fixes: 14dbd1b1e5e5 ("Make registration async")
Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>
Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

Cargo.toml
@@ -7,7 +7,7 @@ authors = ["Florian Kauer <florian.kauer@linutronix.de>"]
 
 [features]
 default = ["dbus", "bpf", "detd", "sysrepo", "netlink"]
-dbus = ["dep:dbus", "dep:dbus-crossroads", "dep:dbus-tokio", "dep:async-shutdown", "dep:async-trait"]
+dbus = ["dep:dbus", "dep:dbus-crossroads", "dep:dbus-tokio", "dep:async-shutdown"]
 bpf = ["dep:libbpf-rs", "dep:libbpf-cargo", "dep:log", "dep:nix"]
 detd = ["dep:bytes", "dep:prost"]
 sysrepo = ["dep:sysrepo", "dep:yang2", "dep:ipnet"]
@@ -16,7 +16,8 @@ libbpf_with_sotoken = []
 
 [dependencies]
 anyhow = "1.0"
-tokio = {version = "1.0", features=["rt", "net", "macros", "sync", "signal"]}
+tokio = {version = "1.0", features=["rt", "net", "macros", "sync", "signal", "time"]}
+async-trait = { version = "0.1.67" }
 eui48 = "1.1.0"
 getrandom = { version = "0.2.8", features = ["std"] }
 env_logger = "0.10.0"
@@ -35,7 +36,6 @@ dbus-crossroads = { git = "https://github.com/diwic/dbus-rs", branch = "master",
 dbus-tokio = { git = "https://github.com/diwic/dbus-rs", branch = "master", optional = true }
 
 async-shutdown = { version = "0.1.2", optional = true }
-async-trait = { version = "0.1.67", optional = true }
 
 ### bpf feature
 libbpf-rs = { version = "0.19", optional = true }

src/configuration/mod.rs
@@ -17,8 +17,8 @@
 //! ```
 //!
 //! With sysrepo configuration (for NETCONF integration):
-//!
-//! ```no_run
+#![cfg_attr(not(feature = "sysrepo"), doc = "```ignore")]
+#![cfg_attr(feature = "sysrepo", doc = "```no_run")]
 //! use detnetctl::configuration::{Configuration, SysrepoConfiguration};
 //! let mut sysrepo_config = SysrepoConfiguration::new()?;
 //! let config = sysrepo_config.get_app_config("app0");

src/guard/mod.rs
@@ -1,6 +1,6 @@
 //! Installs filters to avoid interference between applications
-//!
-//! ```no_run
+#![cfg_attr(not(feature = "bpf"), doc = "```ignore")]
+#![cfg_attr(feature = "bpf", doc = "```no_run")]
 //! use detnetctl::guard::{Guard, BPFGuard};
 //! let mut guard = BPFGuard::new(false);
 //! guard.protect_priority("eth0", 5, 0x9e25b4d41b6c390b)?;

src/interface_setup/mod.rs
@@ -1,6 +1,6 @@
 //! Setup the physical and logical network interfaces
-//!
-//! ```no_run
+#![cfg_attr(not(feature = "netlink"), doc = "```ignore")]
+#![cfg_attr(feature = "netlink", doc = "```no_run")]
 //! use detnetctl::interface_setup::{InterfaceSetup, NetlinkSetup, LinkState};
 //!
 //! # tokio_test::block_on(async {

src/main.rs
@@ -169,9 +169,10 @@ async fn spawn_dbus_service(
 #[cfg(not(feature = "dbus"))]
 async fn spawn_dbus_service(
     _controller: Arc<Mutex<Controller>>,
-    mut _configuration: Box<dyn Configuration + Send>,
-    mut _queue_setup: Box<dyn QueueSetup + Send>,
-    mut _guard: Box<dyn Guard + Send>,
+    _configuration: Arc<Mutex<dyn Configuration + Send>>,
+    _queue_setup: Arc<Mutex<dyn QueueSetup + Send>>,
+    _guard: Arc<Mutex<dyn Guard + Send>>,
+    _interface_setup: Arc<Mutex<dyn InterfaceSetup + Sync + Send>>,
 ) -> Result<()> {
     Err(feature_missing_error("dbus", "--app-name"))
 }

src/queue_setup/mod.rs
@@ -1,6 +1,6 @@
 //! Setup a TSN-capable NIC and qdiscs
-//!
-//! ```no_run
+#![cfg_attr(not(feature = "detd"), doc = "```ignore")]
+#![cfg_attr(feature = "detd", doc = "```no_run")]
 //! use detnetctl::queue_setup::{QueueSetup, DetdGateway};
 //! use detnetctl::configuration::AppConfig;
 //!