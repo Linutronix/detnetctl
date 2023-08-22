1d90193714bce6160d38622b21cd89725c3176cc
Florian Kauer <florian.kauer@linutronix.de>
2023-08-22 04:58:32 -0500

    fix(examples): handle connection loss for simple example
    Until now, the simple example exits as soon as the HTTP
    connection is interrupted (via the SIGPIPE signal).
    
    For longer running demos and to support webservers that
    do not keep the connection alive, reconnect in case
    the socket is closed by the server.
    
    Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>
    Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

--- examples/simple/simple.c
+++ examples/simple/simple.c
@@ -71,77 +71,88 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	/* Setup socket */
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		fprintf(stderr, "Could not create socket\n");
-		return 1;
-	}
-
-	/* Setup socket priority */
-	if (setsockopt(sockfd, SOL_SOCKET, SO_PRIORITY, &priority,
-		       sizeof(priority)) < 0) {
-		fprintf(stderr, "Setting SO_PRIORITY failed\n");
-		close(sockfd);
-		return 1;
-	}
-
-	/* Set socket option to bind to interface */
-	size_t interface_len = strlen(interface);
-	struct ifreq ifr;
-	memcpy(ifr.ifr_name, interface, interface_len);
-	ifr.ifr_name[interface_len] = '\0';
-	if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr,
-		       sizeof(ifr)) < 0) {
-		fprintf(stderr, "Setting SO_BINDTODEVICE failed\n");
-		close(sockfd);
-		return 1;
-	}
-
-	if (token != 0) {
-#ifdef SO_TOKEN
-		// Setup token
-		if (setsockopt(sockfd, SOL_SOCKET, SO_TOKEN, &token,
-			       sizeof(token)) < 0) {
-			fprintf(stderr, "Setting SO_TOKEN failed\n");
-			close(sockfd);
-			return 1;
-		}
-#else
-#warning "Kernel does not support, SO_TOKEN, so it will not be set!\n"
-		fprintf(stderr,
-			"WARNING: SO_TOKEN not available, SO_TOKEN is not set!\n");
-#endif
-	}
-
-	/* Connect to server */
-	struct sockaddr_in server;
-	memcpy(&server.sin_addr, he->h_addr_list[0], he->h_length);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(80);
-
-	if (connect(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-		fprintf(stderr, "Connect error\n");
-		return 1;
-	}
-
-	printf("Connected successfully\n");
-
-	if (signal(SIGINT, sig_int) == SIG_ERR) {
+	/* Register signal handler for SIGINT
+	 * and ignore SIGPIPE to handle closed sockets properly */
+	if (signal(SIGINT, sig_int) == SIG_ERR ||
+	    signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
 		fprintf(stderr, "Can't set signal handler: %s\n",
 			strerror(errno));
 		return 1;
 	}
 
-	char request[100];
-	snprintf(request, sizeof(request),
-		 "HEAD / HTTP/1.1\r\nHost: %s\r\n\r\n\r\n", he->h_name);
-	char response[1000];
-
 	int i = 0;
 	while (!exiting) {
-		if (write(sockfd, request, strlen(request)) >= 0) {
+		/* Setup socket */
+		int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd == -1) {
+			fprintf(stderr, "Could not create socket\n");
+			return 1;
+		}
+
+		/* Setup socket priority */
+		if (setsockopt(sockfd, SOL_SOCKET, SO_PRIORITY, &priority,
+			       sizeof(priority)) < 0) {
+			fprintf(stderr, "Setting SO_PRIORITY failed\n");
+			close(sockfd);
+			return 1;
+		}
+
+		/* Set socket option to bind to interface */
+		size_t interface_len = strlen(interface);
+		struct ifreq ifr;
+		memcpy(ifr.ifr_name, interface, interface_len);
+		ifr.ifr_name[interface_len] = '\0';
+		if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE,
+			       (void *)&ifr, sizeof(ifr)) < 0) {
+			fprintf(stderr, "Setting SO_BINDTODEVICE failed\n");
+			close(sockfd);
+			return 1;
+		}
+
+		if (token != 0) {
+#ifdef SO_TOKEN
+			// Setup token
+			if (setsockopt(sockfd, SOL_SOCKET, SO_TOKEN, &token,
+				       sizeof(token)) < 0) {
+				fprintf(stderr, "Setting SO_TOKEN failed\n");
+				close(sockfd);
+				return 1;
+			}
+#else
+#warning "Kernel does not support, SO_TOKEN, so it will not be set!\n"
+			fprintf(stderr,
+				"WARNING: SO_TOKEN not available, SO_TOKEN is not set!\n");
+#endif
+		}
+
+		/* Connect to server */
+		struct sockaddr_in server;
+		memcpy(&server.sin_addr, he->h_addr_list[0], he->h_length);
+		server.sin_family = AF_INET;
+		server.sin_port = htons(80);
+
+		if (connect(sockfd, (struct sockaddr *)&server,
+			    sizeof(server)) < 0) {
+			fprintf(stderr, "Connect error\n");
+			return 1;
+		}
+
+		printf("Connected successfully\n");
+
+		char request[100];
+		snprintf(request, sizeof(request),
+			 "HEAD / HTTP/1.1\r\nHost: %s\r\n\r\n\r\n", he->h_name);
+		char response[1000];
+
+		while (!exiting) {
+			if (write(sockfd, request, strlen(request)) < 0) {
+				break;
+			}
+
 			int n = read(sockfd, response, sizeof(response));
+			if (n <= 0) {
+				break;
+			}
 			response[n] = '\0';
 
 			/* Not interested in the output for the demo
@@ -149,21 +160,21 @@ int main(int argc, char *argv[])
 			 * 	printf("fputs() error\n");
 			 * }
 			 */
+
+			printf(".");
+			fflush(stdout);
+			sleep(1);
+
+			/* Request and print PTP Status
+			 * Will print nothing if ptp feature is not enabled
+			 */
+			if (i++ % 10 == 9) {
+				print_ptp_status(interface);
+			}
 		}
 
-		printf(".");
-		fflush(stdout);
-		sleep(1);
-
-		/* Request and print PTP Status
-		 * Will print nothing if ptp feature is not enabled
-		 */
-		if (i++ % 10 == 9) {
-			print_ptp_status(interface);
-		}
+		printf("\nConnection closed\n");
 	}
-
-	printf("\n");
 
 	return 0;
 }
