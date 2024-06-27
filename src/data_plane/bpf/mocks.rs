6128ca075e43d414161300fac2395f1b637c914e
Florian Kauer <florian.kauer@linutronix.de>
2024-06-27 16:05:28 +0200

    feat: forwarding via XDP
    As initial step to support bridged applications,
    forward traffic unmodified between the physical
    interface and the virtual interface pair via XDP.
    
    Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>
    Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

---
+++ src/data_plane/bpf/mocks.rs
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2023 Linutronix GmbH
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#![allow(clippy::pedantic, clippy::nursery, single_use_lifetimes, dead_code)]
+use crate::bpf::mocks::MockMap;
+use crate::bpf::mocks::MockXdpProgram;
+use crate::bpf_mock;
+use mockall::mock;
+
+pub(crate) mod bpf_rodata_types {
+    #[derive(Debug, Copy, Clone)]
+    #[repr(C)]
+    pub(crate) struct rodata {
+        pub(crate) debug_output: bool,
+    }
+}
+
+bpf_mock!(
+    DataPlaneSkelBuilder,
+    MockOpenDataPlaneSkel,
+    OpenDataPlaneSkel,
+    MockDataPlaneSkel,
+    DataPlaneSkel,
+    MockDataPlaneProgs,
+    MockDataPlaneMapsMut,
+    MockDataPlaneMaps
+);
+
+mock! {
+    pub(crate) DataPlaneProgs {
+        pub(crate) fn xdp_bridge(&self) -> MockXdpProgram;
+    }
+}
+
+mock! {
+    pub(crate) DataPlaneMapsMut {
+        pub(crate) fn num_streams(&mut self) -> &mut MockMap;
+        pub(crate) fn streams(&mut self) -> &mut MockMap;
+    }
+}
+
+mock! {
+    pub(crate) DataPlaneMaps {
+        pub(crate) fn num_streams(&mut self) -> &mut MockMap;
+        pub(crate) fn streams(&mut self) -> &mut MockMap;
+    }
+}
