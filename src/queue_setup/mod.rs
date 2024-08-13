Linutronix/detnetctl

commit 6a6f9026c1c1e088066fd6c025a73c1c1a7da2de

feat: replace detd by direct TAPRIO setup
Currently, the external detd daemon is used to setup the TAPRIO qdisc.
However, it takes a little different approach to configuration than
detnetctl: detd assumes that it is directly interfaced by the
applications which request setting up the system according to their
application requirements. While this is totally fine for TSN end
stations with homogenous traffic-generating applications that are
all able to communicate their requirements to detd, it gets very
complex when the stream requirements come from different heterogenous
sources (such as some from the applications, some from an external
engineering application, some calculated based on a distributed
algorithm...), because it requires to translate them to a format as
if it would have directly been formulated by an application.

This problem is most obvious when detnetctl shall be used to configure
a router that forwards TSN streams and DetNet flows and does not host
any own application. In this case it is usually easier to directly
calculate the time slot schedule externally and push that to the router,
instead of necessarility involving the router in the end-to-end
stream negotiation. Currently, this would imply mimicing an
artificial application that sends packets of a given size and interval
for every stream. In a similar way, this also applies to containerized
end stations that can be modeled as separate internal end-stations
and an additional virtual bridge or router.

Stream negotiation based on the application requirements is of course
still important, but should be performed on a different layer than
the actual configuration of the schedules within the network hardware.
This split aligns well with the different amendments to IEEE 802.1Q:
IEEE 802.1Qdj is meant to be used for the User/Network Interface (UNI)
for communicating the application requirements, while IEEE 802.1Qcw
is meant for pushing (amongst other things) Qbv schedules to bridges

src/queue_setup/mod.rs
@@ -31,8 +31,9 @@
 //! # Ok::<(), anyhow::Error>(())
 //! ```
 
-use crate::configuration;
+use crate::configuration::TsnInterfaceConfig;
 use anyhow::Result;
+use async_trait::async_trait;
 
 #[cfg(test)]
 use mockall::automock;
@@ -42,60 +43,37 @@ mod taprio;
 #[cfg(feature = "netlink")]
 pub use taprio::TaprioSetup;
 
-/// Configuration returned from the queue setup specifying how to setup the socket
-#[derive(Debug)]
-pub struct QueueSetupResponse {
-    /// Logical interface for the application to bind to (usually a VLAN interface like eth0.2)
-    pub logical_interface: String,
-
-    /// Priority that will be routed to the appropriate qdisc
-    pub priority: u32,
-}
-
 /// Defines how to apply an Ethernet configuration
 #[cfg_attr(test, automock)]
+#[async_trait]
 pub trait QueueSetup {
     /// Apply the given configuration by setting up NIC and qdiscs
     ///
     /// # Errors
     ///
     /// Will return `Err` if the configuration could not be applied,
-    /// e.g. because no connection to `detd` was possible, the
-    /// configuration itself is invalid or `detd` is in a state that
-    /// does not allow applying this configuration.
-    fn apply_config(&self, config: &configuration::AppConfig) -> Result<QueueSetupResponse>;
+    /// e.g. because the configuration itself is invalid or the system
+    /// is in a state that does not allow applying this configuration.
+    async fn apply_config(
+        &self,
+        interface_name: &str,
+        interface_config: &TsnInterfaceConfig,
+    ) -> Result<()>;
 }
-
-#[cfg(feature = "detd")]
-mod detd;
-#[cfg(feature = "detd")]
-pub use detd::DetdGateway;
 
 /// A queue setup doing nothing, but still providing the `QueueSetup` trait
 ///
 /// Useful for testing purposes (e.g. with NICs without TSN capabilities)
 /// or if you only want to use other features without actually configuring the NIC.
-pub struct DummyQueueSetup {
-    priority: u32,
-}
+pub struct DummyQueueSetup;
 
-impl DummyQueueSetup {
-    /// Create new `DummyQueueSetup`
-    ///
-    /// # Arguments
-    ///
-    /// * `priority` - Priority to return from the `apply_config` call
-    #[must_use]
-    pub const fn new(priority: u32) -> Self {
-        Self { priority }
-    }
-}
-
+#[async_trait]
 impl QueueSetup for DummyQueueSetup {
-    fn apply_config(&self, config: &configuration::AppConfig) -> Result<QueueSetupResponse> {
-        Ok(QueueSetupResponse {
-            logical_interface: config.logical_interface()?.clone(),
-            priority: self.priority,
-        })
+    async fn apply_config(
+        &self,
+        _interface_name: &str,
+        _interface_config: &TsnInterfaceConfig,
+    ) -> Result<()> {
+        Ok(())
     }
 }

src/configuration/mod.rs
@@ -144,7 +144,9 @@ pub struct StreamIdentification {
 }
 
 mod schedule;
-pub use schedule::{GateControlEntry, GateOperation, Schedule, ScheduleBuilder};
+pub use schedule::{
+    GateControlEntry, GateControlEntryBuilder, GateOperation, Schedule, ScheduleBuilder,
+};
 
 mod taprio;
 pub use self::taprio::{Clock, Mode, QueueMapping, TaprioConfig, TaprioConfigBuilder};

src/controller/mod.rs
@@ -28,7 +28,7 @@
 //! let cgroup = PathBuf::from("/sys/fs/cgroup/system.slice/some.service/");
 //! let mut configuration = Arc::new(Mutex::new(YAMLConfiguration::new()));
 //! configuration.lock().await.read(File::open(filepath)?)?;
-//! let mut queue_setup = Arc::new(Mutex::new(DummyQueueSetup::new(3)));
+//! let mut queue_setup = Arc::new(Mutex::new(DummyQueueSetup));
 //! let mut dispatcher = Arc::new(Mutex::new(DummyDispatcher));
 //! let mut interface_setup = Arc::new(Mutex::new(DummyInterfaceSetup));
 //! controller
@@ -42,7 +42,7 @@
 //! # Ok::<(), anyhow::Error>(())
 //! ```
 
-use crate::configuration::{AppConfig, Configuration};
+use crate::configuration::{AppConfig, Configuration, FillDefaults, TsnInterfaceConfig};
 use crate::dispatcher::Dispatcher;
 use crate::interface_setup::{InterfaceSetup, LinkState};
 use crate::queue_setup::QueueSetup;
@@ -50,6 +50,7 @@ use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
 use futures::lock::Mutex;
 use options_struct_derive::validate_are_some;
+use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use tokio::time::Instant;
@@ -92,6 +93,16 @@ impl Controller {
     }
 }
 
+async fn fetch_configurations(
+    configuration: Arc<Mutex<dyn Configuration + Send>>,
+) -> Result<(
+    BTreeMap<String, TsnInterfaceConfig>,
+    BTreeMap<String, AppConfig>,
+)> {
+    let mut config = configuration.lock().await;
+    Ok((config.get_interface_configs()?, config.get_app_configs()?))
+}
+
 #[async_trait]
 impl Setup for Controller {
     async fn setup(
@@ -104,37 +115,58 @@ impl Setup for Controller {
         let start = Instant::now();
         println!("Setup of DetNet system");
 
-        // Fetch configurations for apps
-        let app_configs = configuration
-            .lock()
+        // Fetch configurations for interfaces and apps
+        let (mut interface_configs, mut app_configs) = fetch_configurations(configuration.clone())
             .await
-            .get_app_configs()
             .context("Fetching the configuration failed")?;
-        println!("  Fetched from configuration module: {app_configs:#?}");
 
-        for app_config in app_configs.values() {
-            validate_are_some!(app_config, physical_interface, logical_interface)?;
+        for interface_config in interface_configs.values_mut() {
+            interface_config.fill_defaults()?;
+            validate_are_some!(interface_config, schedule, taprio)?;
         }
 
-        for (_app_name, app_config) in app_configs {
+        for app_config in app_configs.values_mut() {
+            app_config.fill_defaults()?;
+            validate_are_some!(
+                app_config,
+                physical_interface,
+                logical_interface,
+                stream,
+                priority
+            )?;
+        }
+
+        println!("  Fetched from configuration module: {interface_configs:#?} {app_configs:#?}");
+
+        // Iterate over all physical interfaces
+        // By this approach, instead of iterating over the app_configs,
+        // we need to setup each interface only once.
+        for (interface_name, interface_config) in interface_configs {
             let locked_interface_setup = interface_setup.lock().await;
 
-            let setup_result = setup(
-                &app_config,
+            let app_configs_for_interface = app_configs.iter().try_fold(
+                BTreeMap::default(),
+                |mut acc, (app_name, app_config)| -> Result<BTreeMap<&String, &AppConfig>> {
+                    if app_config.physical_interface()? == &interface_name {
+                        acc.insert(app_name, app_config);
+                    }
+
+                    Ok(acc)
+                },
+            )?;
+
+            let setup_result = setup_before_interface_up(
+                &interface_name,
+                &interface_config,
+                &app_configs_for_interface,
                 queue_setup.clone(),
                 dispatcher.clone(),
                 &*locked_interface_setup,
             )
             .await; // No ? since we need to ensure that the link is up again even after error
 
-            let if_up_result = set_interfaces_up(
-                vec![
-                    app_config.physical_interface()?,
-                    app_config.logical_interface()?,
-                ],
-                &*locked_interface_setup,
-            )
-            .await;
+            let if_up_result =
+                set_interface_state(&interface_name, LinkState::Up, &*locked_interface_setup).await;
 
             // The first occurred error shall be returned, but a potential second still be printed
             if let Err(if_up_error) = if_up_result {
@@ -148,6 +180,16 @@ impl Setup for Controller {
             }
 
             setup_result?;
+
+            // Set logical interfaces up
+            for app_config in app_configs.values() {
+                set_interface_state(
+                    app_config.logical_interface()?,
+                    LinkState::Up,
+                    &*locked_interface_setup,
+                )
+                .await?;
+            }
         }
 
         println!("  Finished after {:.1?}", start.elapsed());
@@ -190,94 +232,91 @@ impl Protection for Controller {
     }
 }
 
-async fn setup(
-    app_config: &AppConfig,
+async fn setup_before_interface_up(
+    physical_interface: &str,
+    interface_config: &TsnInterfaceConfig,
+    app_configs: &BTreeMap<&String, &AppConfig>,
     queue_setup: Arc<Mutex<dyn QueueSetup + Send>>,
     dispatcher: Arc<Mutex<dyn Dispatcher + Send>>,
     interface_setup: &(dyn InterfaceSetup + Sync + Send),
 ) -> Result<()> {
-    let physical_interface = app_config.physical_interface()?;
-    let logical_interface = app_config.logical_interface()?;
-
-    interface_setup
-        .set_link_state(LinkState::Down, physical_interface)
-        .await
-        .with_context(|| format!("Setting interface {physical_interface} down failed"))?;
-    println!("  Interface {physical_interface} down");
+    set_interface_state(physical_interface, LinkState::Down, interface_setup).await?;
 
     // Setup Queue
-    let queue_setup_response = queue_setup
+    queue_setup
         .lock()
         .await
-        .apply_config(app_config)
+        .apply_config(physical_interface, interface_config)
+        .await
         .context("Setting up the queue failed")?;
-    println!("  Result of queue setup: {queue_setup_response:#?}");
+    println!("  Queues set up");
 
-    // Get stream identification
-    let stream_id = app_config.stream()?;
+    for app_config in app_configs.values() {
+        let logical_interface = app_config.logical_interface()?;
+        let stream_id = app_config.stream()?;
+        let priority = app_config.priority()?;
 
-    // Setup BPF Hooks
-    // It is important to use the physical interface (eth0) and not the logical interface (eth0.2)
-    // here, because otherwise it would be possible to use a different logical interface,
-    // but same SO_PRIORITY and physical interface. Even though it would not be routed to the
-    // same VLAN it could still block the time slot!
-    let mut locked_dispatcher = dispatcher.lock().await;
-    locked_dispatcher
-        .configure_stream(
-            physical_interface,
-            stream_id,
-            queue_setup_response.priority,
-            app_config.pcp_opt().copied(),
-            app_config
-                .cgroup_opt()
-                .map(|c| <PathBuf as AsRef<Path>>::as_ref(c).into()),
-        )
-        .context("Installing protection via the dispatcher failed")?;
-    println!(
-        "  Dispatcher installed for stream {:#?} with priority {} on {}",
-        stream_id, queue_setup_response.priority, physical_interface
-    );
+        // Setup BPF Hooks
+        // It is important to use the physical interface (eth0) and not the logical interface (eth0.2)
+        // here, because otherwise it would be possible to use a different logical interface,
+        // but same SO_PRIORITY and physical interface. Even though it would not be routed to the
+        // same VLAN it could still block the time slot!
+        let mut locked_dispatcher = dispatcher.lock().await;
+        locked_dispatcher
+            .configure_stream(
+                physical_interface,
+                stream_id,
+                (*priority).into(),
+                app_config.pcp_opt().copied(),
+                app_config
+                    .cgroup_opt()
+                    .map(|c| <PathBuf as AsRef<Path>>::as_ref(c).into()),
+            )
+            .context("Installing protection via the dispatcher failed")?;
+        println!(
+            "  Dispatcher installed for stream {stream_id:#?} with priority {priority} on {physical_interface}",
+        );
 
-    if let Some(cgroup) = app_config.cgroup_opt() {
-        println!("  with protection for cgroup {cgroup:?}");
-    }
-
-    // Setup logical interface
-    if let Some(vid) = stream_id.vid_opt() {
-        interface_setup
-            .setup_vlan_interface(physical_interface, logical_interface, *vid)
-            .await
-            .context("Setting up VLAN interface failed")?;
-        println!("  VLAN interface {logical_interface} properly configured");
-    }
-
-    // Add address to logical interface
-    if let Some(addresses) = app_config.addresses_opt() {
-        for (ip, prefix_length) in addresses {
-            interface_setup
-                .add_address(*ip, *prefix_length, logical_interface)
-                .await
-                .context("Adding address to VLAN interface failed")?;
-            println!("  Added {ip}/{prefix_length} to {logical_interface}");
+        if let Some(cgroup) = &app_config.cgroup_opt() {
+            println!("  with protection for cgroup {cgroup:?}");
         }
-    } else {
-        println!("  No IP address configured, since none was provided");
+
+        // Setup logical interface
+        if let Some(vid) = stream_id.vid_opt() {
+            interface_setup
+                .setup_vlan_interface(physical_interface, logical_interface, *vid)
+                .await
+                .context("Setting up VLAN interface failed")?;
+            println!("  VLAN interface {logical_interface} properly configured");
+        }
+
+        // Add address to logical interface
+        if let Some(addresses) = &app_config.addresses_opt() {
+            for (ip, prefix_length) in *addresses {
+                interface_setup
+                    .add_address(*ip, *prefix_length, logical_interface)
+                    .await
+                    .context("Adding address to VLAN interface failed")?;
+                println!("  Added {ip}/{prefix_length} to {logical_interface}");
+            }
+        } else {
+            println!("  No IP address configured, since none was provided");
+        }
     }
 
     Ok(())
 }
 
-async fn set_interfaces_up(
-    interfaces: Vec<&String>,
+async fn set_interface_state(
+    interface: &str,
+    state: LinkState,
     interface_setup: &(dyn InterfaceSetup + Sync + Send),
 ) -> Result<()> {
-    for interface in interfaces {
-        interface_setup
-            .set_link_state(LinkState::Up, interface)
-            .await
-            .with_context(|| format!("Setting interface {interface} up failed"))?;
-        println!("  Interface {interface} up");
-    }
+    interface_setup
+        .set_link_state(state, interface)
+        .await
+        .with_context(|| format!("Setting interface {interface} {state} failed"))?;
+    println!("  Interface {interface} {state}");
 
     Ok(())
 }
@@ -286,15 +325,53 @@ async fn set_interfaces_up(
 mod tests {
     use super::*;
     use crate::configuration::{
-        AppConfig, AppConfigBuilder, MockConfiguration, StreamIdentificationBuilder,
+        AppConfig, AppConfigBuilder, GateControlEntryBuilder, GateOperation, MockConfiguration,
+        Mode, QueueMapping, ScheduleBuilder, StreamIdentificationBuilder, TaprioConfigBuilder,
+        TsnInterfaceConfigBuilder,
     };
     use crate::dispatcher::MockDispatcher;
     use crate::interface_setup::MockInterfaceSetup;
-    use crate::queue_setup::{MockQueueSetup, QueueSetupResponse};
+    use crate::queue_setup::MockQueueSetup;
     use anyhow::anyhow;
     use std::collections::BTreeMap;
     use std::net::{IpAddr, Ipv4Addr};
     use std::path::PathBuf;
+
+    fn generate_interface_config() -> TsnInterfaceConfig {
+        TsnInterfaceConfigBuilder::new()
+            .schedule(
+                ScheduleBuilder::new()
+                    .basetime_ns(10)
+                    .control_list(vec![GateControlEntryBuilder::new()
+                        .operation(GateOperation::SetGates)
+                        .time_interval_ns(1000)
+                        .traffic_classes(vec![1, 2])
+                        .build()])
+                    .number_of_traffic_classes(3)
+                    .priority_map(BTreeMap::from([(0, 1)]))
+                    .build(),
+            )
+            .taprio(
+                TaprioConfigBuilder::new()
+                    .mode(Mode::FullOffload)
+                    .queues(vec![
+                        QueueMapping {
+                            count: 2,
+                            offset: 0,
+                        },
+                        QueueMapping {
+                            count: 1,
+                            offset: 2,
+                        },
+                        QueueMapping {
+                            count: 1,
+                            offset: 3,
+                        },
+                    ])
+                    .build(),
+            )
+            .build()
+    }
 
     fn generate_app_config(interface: String, vid: u16) -> AppConfig {
         let app_config = AppConfigBuilder::new()
@@ -332,6 +409,18 @@ mod tests {
     fn configuration_happy(interface: String, vid: u16) -> MockConfiguration {
         let mut configuration = MockConfiguration::new();
         let interface2 = interface.clone();
+        let interface3 = interface.clone();
+        configuration
+            .expect_get_interface_config()
+            .returning(move |_| Ok(Some(generate_interface_config())));
+        configuration
+            .expect_get_interface_configs()
+            .returning(move || {
+                Ok(BTreeMap::from([(
+                    interface3.clone(),
+                    generate_interface_config(),
+                )]))
+            });
         configuration
             .expect_get_app_config()
             .returning(move |_| Ok(Some(generate_app_config(interface.clone(), vid))));
@@ -347,6 +436,12 @@ mod tests {
     fn configuration_failing() -> MockConfiguration {
         let mut configuration = MockConfiguration::new();
         configuration
+            .expect_get_interface_config()
+            .returning(|_| Err(anyhow!("failed")));
+        configuration
+            .expect_get_interface_configs()
+            .returning(|| Err(anyhow!("failed")));
+        configuration
             .expect_get_app_config()
             .returning(|_| Err(anyhow!("failed")));
         configuration
@@ -355,14 +450,11 @@ mod tests {
         configuration
     }
 
-    fn queue_setup_happy(priority: u32) -> MockQueueSetup {
+    fn queue_setup_happy() -> MockQueueSetup {
         let mut queue_setup = MockQueueSetup::new();
-        queue_setup.expect_apply_config().returning(move |config| {
-            Ok(QueueSetupResponse {
-                logical_interface: config.logical_interface().unwrap().to_string(),
-                priority,
-            })
-        });
+        queue_setup
+            .expect_apply_config()
+            .returning(move |_name, _config| Ok(()));
         queue_setup
     }
 
@@ -370,7 +462,7 @@ mod tests {
         let mut queue_setup = MockQueueSetup::new();
         queue_setup
             .expect_apply_config()
-            .returning(|_| Err(anyhow!("failed")));
+            .returning(|_, _| Err(anyhow!("failed")));
         queue_setup
     }
 
@@ -434,12 +526,11 @@ mod tests {
     async fn test_setup_happy() -> Result<()> {
         let interface = "ethxy";
         let vid = 43;
-        let priority = 32;
         let configuration = Arc::new(Mutex::new(configuration_happy(
             String::from(interface),
             vid,
         )));
-        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(priority)));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy()));
         let dispatcher = Arc::new(Mutex::new(dispatcher_happy()));
         let interface_setup = Arc::new(Mutex::new(interface_setup_happy()));
         let controller = Controller::new();
@@ -453,7 +544,7 @@ mod tests {
     #[should_panic(expected = "Fetching the configuration failed")]
     async fn test_setup_configuration_failure() {
         let configuration = Arc::new(Mutex::new(configuration_failing()));
-        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(32)));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy()));
         let dispatcher = Arc::new(Mutex::new(dispatcher_happy()));
         let interface_setup = Arc::new(Mutex::new(interface_setup_happy()));
         let controller = Controller::new();
@@ -481,7 +572,7 @@ mod tests {
     #[should_panic(expected = "Installing protection via the dispatcher failed")]
     async fn test_setup_dispatcher_failure() {
         let configuration = Arc::new(Mutex::new(configuration_happy(String::from("abc"), 4)));
-        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(32)));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy()));
         let dispatcher = Arc::new(Mutex::new(dispatcher_failing()));
         let interface_setup = Arc::new(Mutex::new(interface_setup_happy()));
         let controller = Controller::new();
@@ -495,7 +586,7 @@ mod tests {
     #[should_panic(expected = "Setting interface abc down failed")]
     async fn test_setup_interface_setup_failure() {
         let configuration = Arc::new(Mutex::new(configuration_happy(String::from("abc"), 4)));
-        let queue_setup = Arc::new(Mutex::new(queue_setup_happy(32)));
+        let queue_setup = Arc::new(Mutex::new(queue_setup_happy()));
         let dispatcher = Arc::new(Mutex::new(dispatcher_happy()));
         let interface_setup = Arc::new(Mutex::new(interface_setup_failing()));
         let controller = Controller::new();

src/facade/mod.rs
@@ -85,7 +85,7 @@
 //! let mut facade = Facade::new(shutdown.clone())?;
 //! let mut configuration = Arc::new(Mutex::new(YAMLConfiguration::new()));
 //! configuration.lock().await.read(File::open(filepath)?)?;
-//! let mut queue_setup = Arc::new(Mutex::new(DummyQueueSetup::new(3)));
+//! let mut queue_setup = Arc::new(Mutex::new(DummyQueueSetup));
 //! let mut dispatcher = Arc::new(Mutex::new(DummyDispatcher));
 //! let mut interface_setup = Arc::new(Mutex::new(DummyInterfaceSetup));
 //! let mut ptp = Arc::new(Mutex::new(PtpManager::new()));

src/interface_setup/mod.rs
@@ -19,18 +19,29 @@
 
 use anyhow::Result;
 use async_trait::async_trait;
+use std::fmt;
 use std::net::IpAddr;
 
 #[cfg(test)]
 use mockall::automock;
 
 /// State of a network link
+#[derive(Clone, Copy, Debug)]
 pub enum LinkState {
     /// Link is active
     Up,
 
     /// Link is inactive
     Down,
+}
+
+impl fmt::Display for LinkState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Up => write!(f, "up"),
+            Self::Down => write!(f, "down"),
+        }
+    }
 }
 
 /// Defines how to setup the link

.github/workflows/github-actions.yml
@@ -16,7 +16,7 @@ jobs:
       - name: Run clippy
         run: |
           # TODO for the moment do not use sysrepo feature, upstream packages are still too old
-          cargo clippy --all-targets --no-default-features --features dbus,bpf,detd,netlink,ptp --color=always -- -D warnings
+          cargo clippy --all-targets --no-default-features --features dbus,bpf,netlink,ptp --color=always -- -D warnings
   reuse:
     runs-on: ubuntu-latest
     steps:

Cargo.toml
@@ -11,10 +11,9 @@ authors = ["Florian Kauer <florian.kauer@linutronix.de>"]
 license = "GPL-3.0-or-later"
 
 [features]
-default = ["dbus", "bpf", "detd", "sysrepo", "netlink", "ptp"]
+default = ["dbus", "bpf", "sysrepo", "netlink", "ptp"]
 dbus = ["dep:dbus", "dep:dbus-crossroads", "dep:dbus-tokio", "dep:async-shutdown"]
 bpf = ["dep:libbpf-rs", "dep:libbpf-cargo"]
-detd = ["dep:bytes", "dep:prost"]
 sysrepo = ["dep:sysrepo", "dep:yang2", "dep:ipnet"]
 netlink = ["dep:rtnetlink","dep:ethtool","dep:netlink-packet-route", "dep:netlink-packet-core"]
 ptp = ["dep:bincode", "dep:ethtool"]
@@ -58,10 +57,6 @@ async-shutdown = { version = "0.1.2", optional = true }
 ### bpf feature
 libbpf-rs = { version = "0.21.2", optional = true }
 
-### detd feature
-bytes = { version = "1.4.0", optional = true }
-prost = { version = "0.11.6", optional = true }
-
 ### sysrepo feature
 sysrepo = { version = "0.4.0", optional = true }
 yang2 = { version = "0.7", optional = true }
@@ -81,9 +76,6 @@ bincode = { version = "1.3.3", optional = true }
 [build-dependencies]
 ### bpf feature
 libbpf-cargo = { version = "0.21.2", optional = true }
-
-### detd feature
-prost-build = "0.11.6"
 
 [dev-dependencies]
 mockall = "0.11.3"

commit 59e7de3adb158b087c5308e0de99e2ac9ba242e4

fix: use TaprioSetup instead of detd in documentation
The use of detd was removed some time ago, but the documentation
has still referenced it. Therefore, adapt to new TaprioSetup.

Fixes: 6a6f9026c1c1 ("feat: replace detd by direct TAPRIO setup")

Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>

src/queue_setup/mod.rs
@@ -3,31 +3,54 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 //
 //! Setup a TSN-capable NIC and qdiscs
-#![cfg_attr(not(feature = "detd"), doc = "```ignore")]
-#![cfg_attr(feature = "detd", doc = "```no_run")]
-//! use detnetctl::queue_setup::{QueueSetup, DetdGateway};
-//! use detnetctl::configuration::{AppConfigBuilder, StreamIdentificationBuilder};
+#![cfg_attr(not(feature = "netlink"), doc = "```ignore")]
+#![cfg_attr(feature = "netlink", doc = "```no_run")]
+//! use detnetctl::queue_setup::{QueueSetup, TaprioSetup};
+//! use std::collections::BTreeMap;
+//! use std::net::{IpAddr, Ipv4Addr};
+//! use detnetctl::configuration::{InterfaceBuilder, GateControlEntryBuilder, ScheduleBuilder,
+//!                                GateOperation, QueueMapping, TaprioConfigBuilder, Mode};
 //!
-//! let app_config = AppConfigBuilder::new()
-//!     .logical_interface("eth0.3".to_owned())
-//!     .physical_interface("eth0".to_owned())
-//!     .period_ns(1000*100)
-//!     .offset_ns(0)
-//!     .size_bytes(1000)
-//!     .stream(
-//!         StreamIdentificationBuilder::new()
-//!         .destination_address("8a:de:82:a1:59:5a".parse()?)
-//!         .vid(3)
-//!         .build()
-//!     )
-//!     .pcp(4)
-//!     .addresses(vec![
-//!         ("192.168.3.3".parse()?, 16)
-//!     ])
-//!     .build();
+//! # tokio_test::block_on(async {
+//! let interface_config = InterfaceBuilder::new()
+//!    .schedule(
+//!        ScheduleBuilder::new()
+//!            .basetime_ns(10)
+//!            .control_list(vec![GateControlEntryBuilder::new()
+//!                .operation(GateOperation::SetGates)
+//!                .time_interval_ns(1000)
+//!                .traffic_classes(vec![1, 2])
+//!                .build()])
+//!            .number_of_traffic_classes(3)
+//!            .priority_map(BTreeMap::from([(0, 1)]))
+//!            .build(),
+//!    )
+//!    .taprio(
+//!        TaprioConfigBuilder::new()
+//!            .mode(Mode::FullOffload)
+//!            .queues(vec![
+//!                QueueMapping {
+//!                    count: 2,
+//!                    offset: 0,
+//!                },
+//!                QueueMapping {
+//!                    count: 1,
+//!                    offset: 2,
+//!                },
+//!                QueueMapping {
+//!                    count: 1,
+//!                    offset: 3,
+//!                },
+//!            ])
+//!            .build(),
+//!    )
+//!    .addresses(vec![(IpAddr::V4(Ipv4Addr::new(192, 168, 3, 3)), 16)])
+//!    .build();
 //!
-//! let mut queue_setup = DetdGateway::new(None, None);
-//! let response = queue_setup.apply_config(&app_config)?;
+//! let mut queue_setup = TaprioSetup;
+//! let response = queue_setup.apply_config("eth0", &interface_config).await?;
+//! # Ok::<(), anyhow::Error>(())
+//! # });
 //! # Ok::<(), anyhow::Error>(())
 //! ```
 